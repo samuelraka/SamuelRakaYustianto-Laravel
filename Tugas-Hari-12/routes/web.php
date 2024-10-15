ca4c4d54b6f53a444c47afd5fcdc80a181790909
samuelraka <139964724+samuelraka@users.noreply.github.com>
2024-10-15 23:27:40 +0700

    commit-update controller

--- Tugas-Hari-12/routes/web.php
+++ Tugas-Hari-12/routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('home');
-})->name('home');
+});
 
 Route::get('/register',[AuthController::class,'index'])->name('register');
 Route::get('/welcome',[AuthController::class,'welcome'])->name('welcome');
 Route::post('/register', [AuthController::class, 'postRegister'])->name('postRegister');
+Route::get('/home',[HomeController::class, 'index'])->name('home');
