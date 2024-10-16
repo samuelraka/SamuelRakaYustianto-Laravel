3f4f6449ec1adf19debeda3160bbf091fefef84d
samuelraka <139964724+samuelraka@users.noreply.github.com>
2024-10-16 23:31:57 +0700

    commit update route

--- Tugas-Hari-13/routes/web.php
+++ Tugas-Hari-13/routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\ContentController;
 
 Route::get('/', function () {
     return view('table');
-});
+})->name('table');
 
 Route::get('/datatable',[ContentController::class,'datatable'])->name('datatable');
